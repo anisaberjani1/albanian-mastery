e73d5e88f93698b44fa9f1d989dfc5417333d78d
anisaberjani1 <anisa.berjani@gmail.com>
2025-06-26 15:48:41 +0200

    The basic Landing page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
+import {
+  ClerkProvider,
+} from "@clerk/nextjs";
 import "./globals.css";
 
 const font = Nunito({
@@ -17,12 +20,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${font.className} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${font.className} antialiased`}>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
