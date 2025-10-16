8713701ebbf19f297b2c48e23ef132fe02c0bdf6
anisaberjani1 <anisa.berjani@gmail.com>
2025-10-16 22:30:43 +0200

    Challenge practice

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,8 @@ import {
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
 import { ExitModal } from "@/components/modals/exit-modal";
+import { HeartsModal } from "@/components/modals/hearts-modal";
+import { PracticeModal } from "@/components/modals/practice-modal";
 
 const font = Nunito({
   subsets: ["latin"],
@@ -27,6 +29,8 @@ export default function RootLayout({
         <body className={`${font.className} antialiased`}>
           <Toaster/>
           <ExitModal/>
+          <HeartsModal/>
+          <PracticeModal/>
           {children}
         </body>
       </html>
